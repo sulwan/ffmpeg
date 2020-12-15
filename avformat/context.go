0eb22cbf1952f6bdf8f9bf3793c77874907551a8
medtrib <medtrib@gmail.com>
2020-12-16 01:21:35 +0800

    修正错误

--- avformat/context.go
+++ avformat/context.go
@@ -50,7 +50,8 @@ func (s *Context) AvNewProgram(id int) *AvProgram {
 
 //Read packets of a media file to get stream information.
 func (s *Context) AvformatFindStreamInfo(d **avutil.Dictionary) int {
-	return int(C.avformat_find_stream_info((*C.struct_AVFormatContext)(s), (**C.struct_AVDictionary)(unsafe.Pointer(d))))
+	return int(C.avformat_find_stream_info((*C.struct_AVFormatContext)(unsafe.Pointer(s)), (**C.struct_AVDictionary)(unsafe.Pointer(d))))
+
 }
 
 //Find the programs which belong to a given stream.
